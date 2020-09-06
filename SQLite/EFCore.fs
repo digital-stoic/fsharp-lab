9b7c4f7e7fe71069afa69a1b214cefe21669bd19
mat <mat@matieux.net>
2020-09-06 20:26:37 +0800

    feat(EntityFrameworkCore): CRUD and Async

--- SQLite/EFCore.fs
+++ SQLite/EFCore.fs
@@ -1,13 +1,15 @@
 module EFCore
 
+open System
 open Microsoft.EntityFrameworkCore
 
 // TODO: Try from DTO type
 // TODO: Try with list and options
-// TODO: automatic primary keys
+// type MyTypeDTO = { Question: string; Answer: int }
+
 [<CLIMutable>]
-type MyType =
-    { Id: System.Nullable<int64>
+type MyTypeEntity =
+    { Id: Nullable<int64>
       Question: string
       Answer: int }
 
@@ -23,7 +25,7 @@ type MyContext =
     //     42
 
     [<DefaultValue>]
-    val mutable myData: DbSet<MyType>
+    val mutable myData: DbSet<MyTypeEntity>
 
     member x.MyData
         with get () = x.myData
@@ -35,7 +37,7 @@ let makeSqliteContext dbFilename () =
     optionsBuilder.UseSqlite(connectionStr) |> ignore
     new MyContext(optionsBuilder.Options)
 
-let saveMyData (context: MyContext) (entity: MyType) =
+let saveMyData (context: MyContext) (entity: MyTypeEntity) =
     async {
         context.MyData.AddAsync(entity).AsTask()
         |> Async.AwaitTask
@@ -46,22 +48,74 @@ let saveMyData (context: MyContext) (entity: MyType) =
         return entity
     }
 
-let saveMyDataSync (context: MyContext) (entity: MyType) =
+let saveMyDataSync (context: MyContext) (entity) =
     context.MyData.Add(entity) |> ignore
     context.SaveChanges true
+
+let getMyDataById (context: MyContext) (id: int) =
+    let _id = Nullable<int64>(int64 id)
+    query {
+        for data in context.MyData do
+            where (data.Id = _id)
+            select data
+            exactlyOne
+    }
+
+// Check https://stackoverflow.com/questions/21097357/async-database-query
+let getMyDataByIdAsync =
+    fun (context: MyContext) (id: int) ->
+        async {
+            let _id = Nullable<int64>(int64 id)
+
+            let result =
+                query {
+                    for data in context.MyData do
+                        where (data.Id = _id)
+                        select data
+                        exactlyOne
+                }
+
+            return result
+        }
+
+let getMyDataById' (context: MyContext) (id: int) =
+    let _id = Nullable<int64>(int64 id)
+    context.MyData.Find(_id)
+
+let updateMyDataById (context: MyContext) (id: int) newAnswer =
+    let _id = Nullable<int64>(int64 id)
+    let data = context.MyData.Find(_id)
+    let newData = { data with Answer = newAnswer }
+    context.Entry(data).CurrentValues.SetValues(newData)
+    context.SaveChanges true |> ignore
 
 let dbFileName = "MyData.db"
 
 let myData1 =
-    { Id = System.Nullable()
+    { Id = Nullable()
       Question = "Universe?"
       Answer = 42 }
+
 
 // TODO: Catch exceptions
 let demo =
     printfn "F# Demo: Entity Framework Core"
     let context = makeSqliteContext dbFileName ()
-    // saveMyData context myData1
-    // |> Async.RunSynchronously
-    saveMyDataSync context myData1 |> ignore
+    // context.Database.Migrate()
+    saveMyData context myData1
+    |> Async.RunSynchronously
+    |> ignore
+    // saveMyDataSync context myData1 |> ignore
+    let id = 3
+    let res1 = getMyDataById context id
+    let res1' = getMyDataById' context id
+
+    let res1'' =
+        getMyDataByIdAsync context id
+        |> Async.RunSynchronously
+
+    printfn "Record Id %d: %A" id res1
+    updateMyDataById context id 9
+    // printfn "Record Id %d: %A" id res1'
+    printfn "Record Id %d: %A" id res1''
     0
