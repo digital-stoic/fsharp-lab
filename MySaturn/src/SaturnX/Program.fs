d8ef9e8a72e2ed6cf4bfe7ead23b68d03a16cf77
mat <mat@matieux.net>
2020-10-03 10:45:52 +0800

    feat(Saturn): First API customization
    - Enable CORS
    - Enable Logging

--- MySaturn/src/SaturnX/Program.fs
+++ MySaturn/src/SaturnX/Program.fs
@@ -2,23 +2,40 @@ module Server
 
 open Saturn
 open Config
+open Microsoft.AspNetCore.Cors.Infrastructure
+open Microsoft.Extensions.Logging
 
-let endpointPipe = pipeline {
-    plug head
-    plug requestId
-}
+let appConfig =
+    {| origin = "https://hoppscotch.io"
+       loggingLevel = LogLevel.Trace |}
 
-let app = application {
-    pipe_through endpointPipe
+let endpointPipe =
+    pipeline {
+        plug head
+        plug requestId
+    }
 
-    error_handler (fun ex _ -> pipeline { render_html (InternalError.layout ex) })
-    use_router Router.appRouter
-    url "http://0.0.0.0:8085/"
-    memory_cache
-    use_static "static"
-    use_gzip
-    use_config (fun _ -> {connectionString = "DataSource=database.sqlite"} ) //TODO: Set development time configuration
-}
+let policyConfig (origin: string) (builder: CorsPolicyBuilder) =
+    builder.WithOrigins(origin).AllowAnyMethod().AllowAnyHeader()
+    |> ignore
+
+let loggingConfig (logLevel: LogLevel) (logging: ILoggingBuilder) =
+    logging.SetMinimumLevel(logLevel) |> ignore
+
+let app =
+    application {
+        pipe_through endpointPipe
+
+        error_handler (fun ex _ -> pipeline { render_html (InternalError.layout ex) })
+        use_router Router.appRouter
+        url "http://0.0.0.0:8085/"
+        memory_cache
+        use_static "static"
+        use_gzip
+        use_config (fun _ -> { connectionString = "DataSource=database.sqlite" }) //TODO: Set development time configuration
+        use_cors appConfig.origin (policyConfig appConfig.origin)
+        logging (loggingConfig appConfig.loggingLevel)
+    }
 
 [<EntryPoint>]
 let main _ =
--- MySaturn/src/SaturnX/Router.fs
+++ MySaturn/src/SaturnX/Router.fs
@@ -3,44 +3,47 @@ module Router
 open Saturn
 open Giraffe.Core
 open Giraffe.ResponseWriters
+open Giraffe.HttpStatusCodeHandlers
 
 
-let browser = pipeline {
-    plug acceptHtml
-    plug putSecureBrowserHeaders
-    plug fetchSession
-    set_header "x-pipeline-type" "Browser"
-}
+let browser =
+    pipeline {
+        plug acceptHtml
+        plug putSecureBrowserHeaders
+        plug fetchSession
+        set_header "x-pipeline-type" "Browser"
+    }
 
-let defaultView = router {
-    get "/" (htmlView Index.layout)
-    get "/index.html" (redirectTo false "/")
-    get "/default.html" (redirectTo false "/")
-}
+let defaultView =
+    router {
+        get "/" (htmlView Index.layout)
+        get "/index.html" (redirectTo false "/")
+        get "/default.html" (redirectTo false "/")
+    }
 
-let browserRouter = router {
-    not_found_handler (htmlView NotFound.layout) //Use the default 404 webpage
-    pipe_through browser //Use the default browser pipeline
+let browserRouter =
+    router {
+        not_found_handler (htmlView NotFound.layout) //Use the default 404 webpage
+        pipe_through browser //Use the default browser pipeline
+        forward "" defaultView
+        forward "/books" Books.Controller.resource
+    }
 
-    forward "" defaultView //Use the default view
-    forward "/books" Books.Controller.resource
-}
+let api =
+    pipeline {
+        plug acceptJson
+        set_header "x-pipeline-type" "Api"
+    }
 
-//Other scopes may use different pipelines and error handlers
+let apiRouter =
+    router {
+        not_found_handler (text "Api 404")
+        pipe_through api
+        get "/test1" (Successful.OK "Test 1 OK")
+    }
 
-// let api = pipeline {
-//     plug acceptJson
-//     set_header "x-pipeline-type" "Api"
-// }
-
-// let apiRouter = router {
-//     not_found_handler (text "Api 404")
-//     pipe_through api
-//
-//     forward "/someApi" someScopeOrController
-// }
-
-let appRouter = router {
-    // forward "/api" apiRouter
-    forward "" browserRouter
-}
+let appRouter =
+    router {
+        forward "" browserRouter
+        forward "/api" apiRouter
+    }
