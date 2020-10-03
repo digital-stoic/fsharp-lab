87c497381c0276a8c7366aa941fc14fa7ce013c2
mat <mat@matieux.net>
2020-10-03 11:56:28 +0800

    feat(Saturn): API call with payload

--- MySaturn/src/SaturnX/Router.fs
+++ MySaturn/src/SaturnX/Router.fs
@@ -4,7 +4,11 @@ open Saturn
 open Giraffe.Core
 open Giraffe.ResponseWriters
 open Giraffe.HttpStatusCodeHandlers
+open Giraffe.ModelBinding
+open FSharp.Control.Tasks.V2.ContextInsensitive
+open Microsoft.AspNetCore.Http
 
+type MyDTO = { question: string; answer: int }
 
 let browser =
     pipeline {
@@ -35,11 +39,28 @@ let api =
         set_header "x-pipeline-type" "Api"
     }
 
+let addId (obj: MyDTO) id =
+    Successful.ok (json ({| obj with id = id |}))
+
+let addPayload (obj: MyDTO): HttpHandler =
+    fun (next: HttpFunc) (ctx: HttpContext) ->
+        task {
+            let! dto = ctx.BindJsonAsync<MyDTO>()
+            printfn "DEBUG: %A" dto
+            return! Successful.ok (json {| obj with payload = dto |}) next ctx
+        }
+
+let obj1 = { question = "universe"; answer = 42 }
+
 let apiRouter =
     router {
         not_found_handler (text "Api 404")
         pipe_through api
         get "/test1" (Successful.OK "Test 1 OK")
+        get "/test2" (Successful.ok (json obj1))
+        getf "/test3/%s" (addId obj1)
+        getf "/test4/%i" (addId obj1)
+        post "/test5" (addPayload obj1)
     }
 
 let appRouter =
