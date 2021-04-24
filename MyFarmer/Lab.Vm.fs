16a0b08cb4ea427506c850c62f458a38f559f299
mat <mat@matieux.net>
2021-04-24 19:49:52 +0800

    feat(Farmer) Base Azure deployments: WebApp

--- MyFarmer/Lab.Vm.fs
+++ MyFarmer/Lab.Vm.fs
@@ -3,16 +3,16 @@ module Lab.Vm
 open Farmer
 open Farmer.Builders
 
-let myVm =
-    vm {
-        name "vm1"
-        username "mat"
-        vm_size Vm.Standard_B1s
-        operating_systema Vm.WindowsServer_2019Datacenter
-        os_disk Vm.StandardSSD_LRS
-    }
+// let myVm =
+//     vm {
+//         name "vm1"
+//         username "mat"
+//         vm_size Vm.Standard_B1s
+//         operating_systema Vm.WindowsServer_2019Datacenter
+//         os_disk Vm.StandardSSD_LRS
+//     }
 
-let deployment = arm { location Lab.Common.locationId }
+// let deployment = arm { location Lab.Common.locationId }
 
-let execDeployment =
-    Lab.Common.executeDeployment Lab.Common.resourceGroupName deployment
+// let execDeployment =
+//     Lab.Common.executeDeployment Lab.Common.resourceGroupName deployment
