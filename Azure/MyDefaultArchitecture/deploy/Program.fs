8148c30d3fdfefffd8b6931960dd8c362861614f
mat <mat@digitalstoic.io>
2022-08-17 21:03:00 +0800

    feat(default-azure-archi): init build and deploy

---
+++ Azure/MyDefaultArchitecture/deploy/Program.fs
@@ -0,0 +1,38 @@
+﻿open System
+open Argu
+
+type DeployTargets =
+    | Build
+    | Test
+    | Clean
+    | NoTarget
+
+type DeployArguments =
+    | [<MainCommand>] Target of DeployTargets
+    interface IArgParserTemplate with
+        member arg.Usage =
+            match arg with
+            | Target _ -> "target [build|test|clean]"
+
+let errorHandler =
+    ProcessExiter(
+        colorizer =
+            function
+            | ErrorCode.HelpText -> None
+            | _ -> Some ConsoleColor.Red
+    )
+
+let parser =
+    ArgumentParser.Create<DeployArguments>(programName = "deploy", errorHandler = errorHandler)
+
+[<EntryPoint>]
+let main argv =
+    let results = parser.ParseCommandLine argv
+
+    match results.GetResult(Target, defaultValue = NoTarget) with
+    | Build -> printfn "build"
+    | Test -> printfn "test"
+    | Clean -> printfn "clean"
+    | NoTarget -> printfn "none"
+
+    0
