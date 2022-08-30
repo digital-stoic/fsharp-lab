9162561ab46beb02df9fba4d293dd8dde2f9189b
mat <mat@digitalstoic.io>
2022-08-30 17:06:48 +0800

    test(default-azure-archi): wrap application layer in infrastructure azure

--- Azure/MyDefaultArchitecture/src/Module1/Infrastructure/Azure/Azure.fs
+++ Azure/MyDefaultArchitecture/src/Module1/Infrastructure/Azure/Azure.fs
@@ -5,13 +5,22 @@ open Microsoft.Azure.WebJobs
 open Microsoft.Azure.WebJobs.Extensions.Http
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Logging
-open MyCompany.MyApp.Common.Domain
+open MyCompany.MyApp.Common
+open MyCompany.MyApp.Common.Application
 
 [<FunctionName("MyCompany-MyApp-Module1-Function1")>]
 let run ([<HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)>] req: HttpRequest) (log: ILogger) =
     async {
         log.LogInformation("MyCompany.MyApp.Module1.Function1 triggered...")
-        let responseMessage = Answer.answer
+
+        let responseMessage =
+            match Domain.Answer.create 42 with
+            | Ok a ->
+                a
+                |> Dto.Answer.fromDomain
+                |> Json.Answer.serialize
+            | Error e -> failwith e
+
         return OkObjectResult(responseMessage) :> IActionResult
     }
     |> Async.StartAsTask
