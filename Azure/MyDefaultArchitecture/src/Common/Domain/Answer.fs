3cde6c74062b82702a4e0df453ca16af884fc3e7
mat <mat@digitalstoic.io>
2022-08-30 17:04:50 +0800

    feat(default-azure-archi): refine common domain for full workflow

--- Azure/MyDefaultArchitecture/src/Common/Domain/Answer.fs
+++ Azure/MyDefaultArchitecture/src/Common/Domain/Answer.fs
@@ -1,5 +1,15 @@
-module MyCompany.MyApp.Common.Domain.Answer
+namespace MyCompany.MyApp.Common.Domain
 
-type Answer = { value: int }
+type Answer = private Answer of int
 
-let answer = { value = 42 }
+module Answer =
+    let create answer =
+        match answer with
+        | 42 -> 42 |> Answer |> Ok
+        | _ ->
+            let msg = "[Answer.create] There is only one possible answer"
+            Error msg
+
+    let value (Answer answer) = answer
+
+    let wrong = 0 |> Answer
