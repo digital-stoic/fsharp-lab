b82fbee6f236e616c12df337a1c4e0cfd8bec2d2
mat <mat@digitalstoic.io>
2023-03-13 21:28:11 +0800

    test(my-company-app): Dockerfile build - WIP

--- MyCompany.MyApp/src/MyOrder/Test/Unit/Application.fs
+++ MyCompany.MyApp/src/MyOrder/Test/Unit/Application.fs
@@ -1,15 +1,14 @@
-﻿module MyCompany.MyOrder.Test.Unit.Application
-//namespace MyCompany.MyOrder.Test.Unit
+﻿namespace MyCompany.MyOrder.Test.Unit
 
 open NUnit.Framework
 open Expecto.Flip.Expect
 
-//module Application =
-[<SetUp>]
-let Setup () = ()
+module Application =
+    [<SetUp>]
+    let Setup () = ()
 
-[<Test>]
-let ultimateAnswerIs42 =
-    let expected = 42
-    let actual = 42
-    expected |> equal "Answer value is 42" actual
+    [<Test>]
+    let ultimateAnswerIs42 () =
+        let expected = 42
+        let actual = 42
+        expected |> equal "Answer value is 42" actual
